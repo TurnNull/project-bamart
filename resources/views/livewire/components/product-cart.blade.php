82e543d27e45485a23c724cbcc4cb80b092e31d0
TurnNull <husain.7605@gmail.com>
2025-12-06 23:18:26 +0800

    total keranjang fix#0.6.9

--- resources/views/livewire/components/product-cart.blade.php
+++ resources/views/livewire/components/product-cart.blade.php
@@ -1,5 +1,5 @@
 <div class="px-2">
     <a href="{{ route('items.cart') }}" class="">
-        <small>{{ $cartTotal }}</small><x-icon name="shopping-cart" />
+        <small>{{ ($cartTotal) ? $cartTotal : '' }}</small><x-icon name="shopping-cart" />
     </a>
 </div>
